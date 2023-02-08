f76458410d6fd513b176ef06070fc6b43f4ff9b5
Jaylan Igbinoba <jigbinoba@ggc.edu>
2023-02-08 15:00:18 -0500

    1st problem done

---
+++ src/BMI.java
@@ -0,0 +1,19 @@
+/**
+ * Class: Triangle
+ * @author: Jaylan Igbinoba
+ * @Course: ITEC 2140 Spring 2023
+ * @verison: 1.0
+ * @Written: February 8, 2023
+ * @Description: This Code tries to calculate the perimeter of the triangle.
+ */
+
+import java.util.Scanner;
+public class BMI {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please input your weight: ");
+
+
+    }
+
+}
